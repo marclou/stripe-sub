0890abcf2790d589cd3869c57ae1cb16971ad310
Marc Lou <marc.louvion@gmail.com>
2023-08-24 15:21:39 +0800

    add api

---
+++ libs/api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+// use this to interact with our own API (nextJS /api folder) from the front-end side
+const apiClient = axios.create({
+  baseURL: "/api",
+});
+
+apiClient.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    let message = "";
+
+    if (error.response?.status === 401) {
+      // User not auth, ask to re login
+      toast.error("Please login");
+      return signIn(undefined, { callbackUrl: "/" });
+    } else if (error.response?.status === 403) {
+      // User not authorized, must subscribe/purchase/pick a plan
+      message = "Pick a plan to use this feature";
+    } else {
+      message =
+        error?.response?.data?.error || error.message || error.toString();
+    }
+
+    error.message =
+      typeof message === "string" ? message : JSON.stringify(message);
+
+    console.error(error.message);
+
+    if (error.message) {
+      toast.error(error.message);
+    } else {
+      toast.error("something went wrong...");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
