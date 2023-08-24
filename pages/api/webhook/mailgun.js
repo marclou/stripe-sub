d7a541b8df24795e9edc891398b1e70457dd7a01
Marc Lou <marc.louvion@gmail.com>
2023-08-24 14:44:46 +0800

    add mailgun forwarder

---
+++ pages/api/webhook/mailgun.js
@@ -0,0 +1,36 @@
+import { sendEmail } from "@/libs/email";
+import config from "@/config";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST": {
+      const emailData = req.body;
+
+      try {
+        // extract the email content, subject and sender
+        const { "stripped-text": strippedText, subject, sender } = emailData;
+
+        if ((strippedText, subject, sender)) {
+          // send email to the admin
+          await sendEmail(
+            config.mailgun.forwardRepliesTo,
+            `${config.appName} | ${subject}`,
+            `Subject: ${subject}\n\nFrom: ${sender}\n\nContent:\n${strippedText}`,
+            `<div><p>Subject: ${subject}</p><p>From: ${sender}</p><p>Content:</p><p>${strippedText}</p></div>`,
+            sender
+          );
+        }
+
+        return res.status(200).send();
+      } catch (e) {
+        console.error(e?.message);
+
+        return res.status(500).send();
+      }
+    }
+    default:
+      return res.status(200).send();
+  }
+}
