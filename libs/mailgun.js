fc4042d09b60fea15e1138fb69e7c59564d41ff5
Marc Lou <marc.louvion@gmail.com>
2023-08-24 14:48:32 +0800

    fix

--- libs/mailgun.js
+++ libs/mailgun.js
@@ -2,6 +2,7 @@ import config from "@/config";
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
+
 const mg = mailgun.client({
   username: "api",
   key: process.env.MAILGUN_API_KEY,
@@ -9,7 +10,7 @@ const mg = mailgun.client({
 
 export const sendEmail = async (to, subject, text, html, replyTo) => {
   const data = {
-    from: `Marc at WorkbookPDF <${config.email.fromMe}>`,
+    from: config.mailgun.fromAdmin,
     to: [to],
     subject,
     text,
