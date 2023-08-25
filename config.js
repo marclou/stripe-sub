60bd4e65ec178ce7229d615e35703351663f42bb
Marc Lou <marc.louvion@gmail.com>
2023-08-25 20:11:27 +0800

    details

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ const config = {
     fromNoReply: `ShipFast <noreply@mg.shipfa.st>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
     fromAdmin: `Marc at ShipFast <marc@mg.shipfa.st>`,
-    // Email used when your used need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support.
+    // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
     supportEmail: "marc@mg.shipfa.st",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
     forwardRepliesTo: "marc.louvion@gmail.com",
