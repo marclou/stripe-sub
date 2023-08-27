ccffe8a2aad2a7515c0a16ba6ff59afd662ce2ed
Marc Lou <marc.louvion@gmail.com>
2023-08-27 09:24:53 +0800

    details

--- config.js
+++ config.js
@@ -49,11 +49,10 @@ const config = {
   },
   colors: {
     // REQUIRED — The DaisyUI theme to use (added to _document.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
-    theme: "dark",
-    // REQUIRED — This colors will be reflected on the whole app (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme. HEX only.
-    main: themes[`[data-theme=dark]`]["primary"],
-    // OR you can just do this to use a custom color:
-    // main: "#f37055",
+    theme: "light",
+    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
+    // OR you can just do this to use a custom color: main: "#f37055". HEX only.
+    main: themes[`[data-theme=light]`]["primary"],
   },
   // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in appiClient (/libs/api.js) upon 401 errors from our API & /hooks/usePrivate.js
   callbackUrl: "/dashboard",
