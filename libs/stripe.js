25df3ebf5afe81be2be22edf51657ddd268cd441
Marc Lou <marc.louvion@gmail.com>
2023-08-28 21:33:10 +0800

    fix coupon

--- libs/stripe.js
+++ libs/stripe.js
@@ -40,7 +40,7 @@ export const createCheckout = async ({
       discounts: couponId
         ? [
             {
-              couponId,
+              coupon: couponId,
             },
           ]
         : [],
