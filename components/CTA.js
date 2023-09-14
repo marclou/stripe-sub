4802dcf77d7715e99175bde3ac0645812d1f7034
Marc Lou <marc.louvion@gmail.com>
2023-09-14 13:49:59 +0800

    (fix) legacy prop

--- components/CTA.js
+++ components/CTA.js
@@ -6,9 +6,8 @@ const CTA = () => {
       <Image
         src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
         alt="Background"
-        className="w-full"
+        className="object-cover w-full"
         fill
-        objectFit="cover"
       />
       <div className="relative hero-overlay bg-neutral bg-opacity-70"></div>
       <div className="relative hero-content text-center text-neutral-content p-8">
