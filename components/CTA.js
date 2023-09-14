e5d6bbb02728fdf3826b4484930065176c083a15
Marc Lou <marc.louvion@gmail.com>
2023-09-14 10:43:00 +0800

    (fix) align center

--- components/CTA.js
+++ components/CTA.js
@@ -12,7 +12,7 @@ const CTA = () => {
       />
       <div className="relative hero-overlay bg-neutral bg-opacity-70"></div>
       <div className="relative hero-content text-center text-neutral-content p-8">
-        <div className="max-w-xl p-8 md:p-0">
+        <div className="flex flex-col items-center max-w-xl p-8 md:p-0">
           <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12">
             Boost your app, launch, earn
           </h2>
