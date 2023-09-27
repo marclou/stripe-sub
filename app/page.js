440c4ad15ea91f551408372bd12d844b0b6d03ed
Marc Lou <marc.louvion@gmail.com>
2023-09-27 12:32:52 +0800

    fix

--- app/page.js
+++ app/page.js
@@ -1,49 +1,33 @@
-"use client";
-
-import { useState } from "react";
-import apiClient from "@/libs/api";
-
-import { signIn } from "next-auth/react";
-import config from "@/config";
-
-const SigninButton = () => {
+export default function Page() {
   return (
-    <button
-      className="btn btn-primary"
-      onClick={() =>
-        signIn(undefined, { callbackUrl: config.auth.callbackUrl })
-      }
-    >
-      Login
-    </button>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center text-center gap-12 p-8">
+        <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
+
+        <p className="text-lg opacity-80">
+          The start of your new startup... What are you gonna build?
+        </p>
+
+        <a
+          className="btn btn-primary"
+          href="https://shipfa.st/docs"
+          target="_blank"
+        >
+          Documentation & tutorials{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
+      </main>
+    </>
   );
-};
-
-const UserProfile = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const saveUser = async () => {
-    setIsLoading(true);
-
-    try {
-      const { data } = await apiClient.post("/user", {
-        email: "new@gmail.com",
-      });
-
-      console.log(data);
-    } catch (e) {
-      console.error(e?.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  return (
-    <div className="p-12">
-      <h2>hey</h2>
-      <SigninButton />
-    </div>
-  );
-};
-
-export default UserProfile;
+}
