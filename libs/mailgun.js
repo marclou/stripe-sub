6c7ac0d7922956f9e9a98517f9cf9ce602d1fb0c
Marc Lou <marc.louvion@gmail.com>
2023-10-02 21:42:01 +0800

    (fix)

--- libs/mailgun.js
+++ libs/mailgun.js
@@ -5,7 +5,7 @@ const mailgun = new Mailgun(formData);
 
 const mg = mailgun.client({
   username: "api",
-  key: process.env.MAILGUN_API_KEY,
+  key: process.env.MAILGUN_API_KEY || "dummy",
 });
 
 /**
