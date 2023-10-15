95b6a061883d07d2eaae1653d3cb6ac0b9c3f6e8
Marc Lou <marc.louvion@gmail.com>
2023-10-15 15:32:27 +0800

    (fix) typo

--- config.js
+++ config.js
@@ -83,7 +83,7 @@ const config = {
     forwardRepliesTo: "marc.louvion@gmail.com",
   },
   colors: {
-    // REQUIRED — The DaisyUI theme to use (added to _document.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
+    // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
     theme: "light",
     // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
     // OR you can just do this to use a custom color: main: "#f37055". HEX only.
