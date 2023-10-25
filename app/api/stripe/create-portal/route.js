866aac70053471a15435cda26437b585be78f9f0
Marc Lou <marc.louvion@gmail.com>
2023-10-25 14:34:06 +0800

    (new) 💳 ButtonAccount: let users manage their accounts (billing, sign out)

---
+++ app/api/stripe/create-portal/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/libs/next-auth";
+import connectMongo from "@/libs/mongoose";
+import { createCustomerPortal } from "@/libs/stripe";
+import User from "@/models/User";
+
+export async function POST(req) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    try {
+      await connectMongo();
+
+      const body = await req.json();
+
+      const { id } = session.user;
+
+      const user = await User.findById(id);
+
+      if (!user?.customerId) {
+        return NextResponse.json(
+          {
+            error:
+              "You don't have a billing account yet. Make a purchase first.",
+          },
+          { status: 400 }
+        );
+      } else if (!body.returnUrl) {
+        return NextResponse.json(
+          { error: "Return URL is required" },
+          { status: 400 }
+        );
+      }
+
+      const stripePortalUrl = await createCustomerPortal({
+        customerId: user.customerId,
+        returnUrl: body.returnUrl,
+      });
+
+      return NextResponse.json({
+        url: stripePortalUrl,
+      });
+    } catch (e) {
+      console.error(e);
+      return NextResponse.json({ error: e?.message }, { status: 500 });
+    }
+  } else {
+    // Not Signed in
+    return NextResponse.json({ error: "Not signed in" }, { status: 401 });
+  }
+}
