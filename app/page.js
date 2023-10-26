d9ecd7c4363072ffb01dfe4da8e6dc2597be50fc
Marc Lou <marc.louvion@gmail.com>
2023-10-26 11:15:25 +0800

    (new) default templates come with signup/user-account buttons

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import ButtonSignin from "@/components/ButtonSignin";
+
 export default function Page() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center text-center gap-12 p-8">
+      <header className="p-4 flex justify-end max-w-7xl mx-auto">
+        <ButtonSignin text="Login" />
+      </header>
+      <main className="flex flex-col items-center justify-center text-center gap-12 px-8 py-24">
         <h1 className="text-3xl font-extrabold">Ship Fast ⚡️</h1>
 
         <p className="text-lg opacity-80">
