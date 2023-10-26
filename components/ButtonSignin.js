84d0a10a7a5e9be08f1ef1183f16db57f9106f0a
Marc Lou <marc.louvion@gmail.com>
2023-10-26 11:28:20 +0800

    (update) automatically display user account & redirect to private dashboard if user is logged in

--- components/ButtonSignin.js
+++ components/ButtonSignin.js
@@ -1,19 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { signIn } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import config from "@/config";
 
 // A simple button to sign in with our providers (Google & Magic Links).
 // It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
+// If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
 const ButtonSignin = ({ text = "Get started", extraStyle }) => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  const handleClick = () => {
+    if (status === "authenticated") {
+      router.push(config.auth.callbackUrl);
+    } else {
+      signIn(undefined, { callbackUrl: config.auth.callbackUrl });
+    }
+  };
+
   return (
     <button
       className={`btn ${extraStyle ? extraStyle : ""}`}
-      onClick={() =>
-        signIn(undefined, { callbackUrl: config.auth.callbackUrl })
-      }
+      onClick={handleClick}
     >
-      {text}
+      {status === "authenticated" ? (
+        <>
+          {session.user?.image ? (
+            <img
+              src={session.user?.image}
+              alt={session.user?.name || "Account"}
+              className="w-6 h-6 rounded-full shrink-0"
+              referrerPolicy="no-referrer"
+            />
+          ) : (
+            <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
+              {session.user?.name?.charAt(0) || session.user?.email?.charAt(0)}
+            </span>
+          )}
+          {session.user?.name || session.user?.email || "Account"}
+        </>
+      ) : (
+        text
+      )}
     </button>
   );
 };
