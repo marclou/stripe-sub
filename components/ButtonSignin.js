c6c881d8a1ee8923fed30d475b7f3c2211a67617
Marc Lou <marc.louvion@gmail.com>
2023-11-22 07:58:38 +0800

    (minor) link to dashboard instead of router push (it's faster)

--- components/ButtonSignin.js
+++ components/ButtonSignin.js
@@ -2,6 +2,7 @@
 "use client";
 
 import { useSession, signIn } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import config from "@/config";
 
@@ -20,32 +21,37 @@ const ButtonSignin = ({ text = "Get started", extraStyle }) => {
     }
   };
 
+  if (status === "authenticated") {
+    return (
+      <Link
+        href={config.auth.callbackUrl}
+        className={`btn ${extraStyle ? extraStyle : ""}`}
+      >
+        {session.user?.image ? (
+          <img
+            src={session.user?.image}
+            alt={session.user?.name || "Account"}
+            className="w-6 h-6 rounded-full shrink-0"
+            referrerPolicy="no-referrer"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
+            {session.user?.name?.charAt(0) || session.user?.email?.charAt(0)}
+          </span>
+        )}
+        {session.user?.name || session.user?.email || "Account"}
+      </Link>
+    );
+  }
+
   return (
     <button
       className={`btn ${extraStyle ? extraStyle : ""}`}
       onClick={handleClick}
     >
-      {status === "authenticated" ? (
-        <>
-          {session.user?.image ? (
-            <img
-              src={session.user?.image}
-              alt={session.user?.name || "Account"}
-              className="w-6 h-6 rounded-full shrink-0"
-              referrerPolicy="no-referrer"
-              width={24}
-              height={24}
-            />
-          ) : (
-            <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
-              {session.user?.name?.charAt(0) || session.user?.email?.charAt(0)}
-            </span>
-          )}
-          {session.user?.name || session.user?.email || "Account"}
-        </>
-      ) : (
-        text
-      )}
+      {text}
     </button>
   );
 };
