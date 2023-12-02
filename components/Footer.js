60d13f639eb87440aeefc89c742e7e0f70b5b85d
Marc Lou <marc.louvion@gmail.com>
2023-12-02 15:24:01 +0800

    (minor) add footer link to blog

--- components/Footer.js
+++ components/Footer.js
@@ -57,6 +57,9 @@ const Footer = () => {
                 <Link href="/#pricing" className="link link-hover">
                   Pricing
                 </Link>
+                <Link href="/blog" className="link link-hover">
+                  Blog
+                </Link>
                 <a href="/#" target="_blank" className="link link-hover">
                   Affiliates
                 </a>
