0e22db38f082d57f0384a4c9035284a729e59621
Marc Lou <marc.louvion@gmail.com>
2023-12-18 19:58:26 +0800

    (new) Problem Agitation

--- components/Problem.js
+++ components/Problem.js
@@ -44,7 +44,7 @@ const Problem = () => {
     <section className="bg-neutral text-neutral-content">
       <div className="max-w-7xl mx-auto px-8 py-16 md:py-32 text-center">
         <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight mb-6 md:mb-8">
-          80% of startups fail because they never launch
+          80% of startups fail because founders never launch
         </h2>
         <p className="max-w-xl mx-auto text-lg opacity-90 leading-relaxed mb-12 md:mb-20">
           Emails, DNS records, user authentication... There&apos;s so much going
