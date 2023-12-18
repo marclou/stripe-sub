ac6f97097a6568e9b9a2f56dde2d29f20e6956f2
Marc Lou <marc.louvion@gmail.com>
2023-12-18 20:14:46 +0800

    (minor) copy

--- components/FeaturesAccordion.js
+++ components/FeaturesAccordion.js
@@ -196,9 +196,9 @@ const FeaturesAccordion = () => {
     >
       <div className="px-8">
         <h2 className="font-extrabold text-4xl lg:text-6xl tracking-tight mb-12 md:mb-24">
-          All you need to ship your startup in days,
+          All you need to ship your startup fast
           <span className="bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 leading-relaxed whitespace-nowrap">
-            not months
+            and get profitable
           </span>
         </h2>
         <div className=" flex flex-col md:flex-row gap-12 md:gap-24">
