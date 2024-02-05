e77067e1e5fec69b16a040600b7fac74d07ea39e
Marc Lou <marc.louvion@gmail.com>
2024-02-05 19:56:24 +0800

    fix: noindex on blog

--- app/blog/layout.js
+++ app/blog/layout.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import HeaderBlog from "./_assets/components/HeaderBlog";
 import Footer from "@/components/Footer";
 
 export default async function LayoutBlog({ children }) {
   return (
     <div>
-      <HeaderBlog />
+      <Suspense>
+        <HeaderBlog />
+      </Suspense>
 
       <main className="min-h-screen max-w-6xl mx-auto p-8">{children}</main>
 
